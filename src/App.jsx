9fac67f8f7ec21b467426fc1d532ca0ba704dde3
Vo Le Tinh Nhi <81817947+Az-Ru@users.noreply.github.com>
2025-04-27 09:19:29 +0000

    set homepage for github portfolio

--- src/App.jsx
+++ src/App.jsx
@@ -24,12 +24,12 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
+  name: "Le Tinh Nhi Vo",
   title: "Web Designer & Content Creator",
-  email: "alex@example.com",
+  email: "voletinhnhi@gmail.com",
   gitHub: "microsoft",
   instagram: "microsoft",
-  linkedIn: "satyanadella",
+  linkedIn: "letinh-nhi-vo",
   medium: "",
   twitter: "microsoft",
   youTube: "Code",
